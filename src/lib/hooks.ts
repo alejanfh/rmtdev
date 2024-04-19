3327a35d04bb50bbd148bf36fe17fbe2bf28d77d
Alejandro Fortes Hidalgo <afortes@eunoia.es>
2024-04-19 12:34:41 +0200

    Job detail fetch y loading

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,5 +1,26 @@
 import { useEffect, useState } from 'react'
-import { JobItem } from './types'
+import { JobItem, JobItemExpanded } from './types'
+import { BASE_API_URL } from './constants'
+
+export function useActiveId() {
+  const [activeId, setActiveId] = useState<number | null>(null)
+
+  useEffect(() => {
+    const handleHashChange = () => {
+      const id = +window.location.hash.slice(1)
+      setActiveId(id)
+    }
+    handleHashChange()
+
+    window.addEventListener('hashchange', handleHashChange)
+
+    return () => {
+      window.removeEventListener('hashchange', handleHashChange)
+    }
+  }, [])
+
+  return activeId
+}
 
 export function useJobItems(searchText: string) {
   const [jobItems, setJobItems] = useState<JobItem[]>([])
@@ -12,9 +33,7 @@ export function useJobItems(searchText: string) {
 
     const fetchData = async () => {
       setIsLoading(true)
-      const response = await fetch(
-        `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
-      )
+      const response = await fetch(`${BASE_API_URL}?search=${searchText}`)
 
       const data = await response.json()
       setIsLoading(false)
@@ -25,4 +44,25 @@ export function useJobItems(searchText: string) {
   }, [searchText])
 
   return [jobItemsSliced, isLoading] as const
+}
+
+export function useJobItem(id: number | null) {
+  const [jobItem, setJobItem] = useState<JobItemExpanded | null>(null)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (!id) return
+
+    const fetchData = async () => {
+      setIsLoading(true)
+      const response = await fetch(`${BASE_API_URL}/${id}`)
+      const data = await response.json()
+      setIsLoading(false)
+      setJobItem(data.jobItem)
+    }
+
+    fetchData()
+  }, [id])
+
+  return [jobItem, isLoading] as const
 }
